9ac9b263d66c85cce19044c3e6300bf52423da90
Ezra <ezrazigmond@gmail.com>
2014-11-29 20:48:26 -0500

    Commented code and added error checking to geoip

--- main.py
+++ main.py
@@ -4,43 +4,55 @@ from time import sleep
 import RPi.GPIO as GPIO
 from math import *
 
+# GPIO pins for servo
 SERVOPINLEFT = 11
 SERVOPINRIGHT = 13
 SERVOPINLIFT = 15
 
+# Lift commands
 UP = 0
 DOWN = 1
 WIPE = 2
 
-#miliseconds
+# Positions for various leves of lifting
 LEVELWRITE = TODO
 LEVELUP = TODO
 LEVELWIPE = TODO
 
+# determines speed of the servo, higher is slower
 LIFTSPEED = 1500
 
 # Set pin numbering mode
 GPIO.setmode(GPIO.BOARD)
 
 # Set servo pins as output
-GPIO.setup(SERVOLEFT,GPIO.OUT)
-GPIO.setup(SERVOLEFT,GPIO.OUT)
-GPIO.setup(SERVOLEFT,GPIO.OUT)
+GPIO.setup(SERVOPINLEFT,GPIO.OUT)
+GPIO.setup(SERVOPINRIGHT,GPIO.OUT)
+GPIO.setup(SERVOPINLIFT,GPIO.OUT)
 
 # Set up the pins for PWM at 50 HZ (meaning 20 ms periods)
 leftservo = GPIO.PWM(SERVOPINLEFT, 50)
 rightservo = GPIO.PWM(SERVOPINRIGHT, 50)
 liftservo = GPIO.PWM(SERVOPINLIFT, 50)
 
+# keeps track of location of pen
 currentX = TODO
 currentY = TODO
 
+# Keeps track of the lift position of the pen
 servoLift = TODO
 
 def wipe():
-	lift(WIPE)
+	"""gets the eraser and then clears the board"""
 
-def drawnum(num):
+	lift(WIPE)
+	TODO
+
+# given a number as a string (or a decimal point)
+def drawNum(num):
+	"""Given a digit or . as a string, writes the digit by lifting up the pen,
+	going to the appropriate location, putting the pen down, and then writing"""
+
 	if num == '0':
 		lift(UP)
 	elif num == '1':
@@ -65,9 +77,16 @@ def drawnum(num):
 		lift(UP)
 
 def getDigits(temp):
+	"""Given the temp as a float, returns an array of the characters
+	representing the digits (and the decimal point"""
+
 	return list(str(temp))
 
 def lift(level):
+	"""Given the level UP, DOWN, or WIPE, raises or lowers the pen
+	to the appropriate point based on the current lift position stored
+	in servoLift"""
+
 	if level == UP:
 		if servoHeight >= LEVELUP:
 			while servoHeight >= LEVELUP:
@@ -76,7 +95,7 @@ def lift(level):
 				delayMicroseconds(LIFTSPEED)
 
 		else:
-			while servoLift <= LEVELUP:
+			while servoLift <= LEVELUP: 
 				servoLift += 1
 				writeMicroseconds(liftServo, servoLift)
 				delayMicroseconds(LIFTSPEED)
@@ -107,13 +126,18 @@ def lift(level):
 				writeMicroseconds(liftServo, servoLift)
 				delayMicroseconds(LIFTSPEED)
 
-def setDest(x, y):
+def setDestination(x, y):
+	"""Given a destination x,y, calls goToXY in a loop so that a straight
+	is drawn between currentX, currentY and the destination"""
+
 	dx = x - currentX
 	dy = y - currentY
 
+	# use distance formula
 	distance = sqrt(dx*dx + dy*dy)
 	steps = TODO * distance #how many steps per unit?
 
+	# break the 
 	for i in range(0,steps):
 		goToXY(currentX+dx/steps,currentY+dy/steps)
 		currentX += dx/steps
@@ -121,16 +145,25 @@ def setDest(x, y):
 
 
 def goToXY(x, y):
+	"""Given an x,y points, determines the current number of microseconds to
+	write to the left servo and the right servo"""
+
+	TODO
 	#How to physics?
 
 def writeMicroseconds(servo, microseconds):
+	"""Calculates duty cycle based on desired pulse width"""
 	servo.ChangeDutyCycle(microsends/200)
 
 def delayMicroseconds(microseconds):
+	"""Coverts microsecond delay to seconds delay"""
 	sleep(microseconds/1000000)
 
 while (1):
 	weatherGetter = Weather()
 	temp = weatherGetter.getWeather()
-	print(getDigits(temp))
+	digits = getDigist(temp)
+	print(digits)
+	for i in range(1,len(digits)):
+		drawNum(digits[i])
 	break;
--- weather.py
+++ weather.py
@@ -4,16 +4,25 @@ import json
 import urllib2
 
 def toFahrenheit(temp):
+	"""Converts from kelvin to fahrenheit"""
+
 	return (float(temp) - 273.15) * 1.8 + 32
 
 class Weather:
 
 	def __init__(self):
+		"""Gets location based on IP address (may be problematic on Harvard network)"""
+
 		f = urllib2.urlopen('http://freegeoip.net/json/')
 		geoJSON = f.read()
 		f.close()
 		geo = json.loads(geoJSON)
 		self.location = geo['city'] + ',' + geo['region_code']
+		
+		#If something went wrong with the location fetching,
+		# assume we're in Cambridge (our fair city)
+		if self.location == ',':
+			self.location = "Cambridge,MA"
 
 	def getWeather(self):
 		address = 'http://api.openweathermap.org/data/2.5/weather?q=' + self.location + '&APPID=' + APIKEY
