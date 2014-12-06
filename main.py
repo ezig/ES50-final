a29f4971fad040404b9362723b7ea212cb06b711
Ezra <ezrazigmond@gmail.com>
2014-12-06 17:12:01 -0500

    test numbers

--- main.py
+++ main.py
@@ -135,9 +135,9 @@ def drawNum(num, x, y):
 	# elif num == '.':
 	# 	lift(UP)
 	if num == 0:
-		linePath(x + 12.0, y + 6.0)
+		# linePath(x + 12.0, y + 6.0)
     	lift(0)
-    	arcPath(x + 7.0, y + 10.0, 10.0, -0.8, 6.7, 'Counterclockwise')
+    	arcPath(x + 7.0, y + 10.0, 15.0, -0.8, 6.7, 'Counterclockwise')
     	lift(1)
 	if num == 1:
 		linePath(x + 3.0, y + 15.0)
